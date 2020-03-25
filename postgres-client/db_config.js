89416b2d8a38d42ebebdcda5f21e6dba6d472297
LeonibelDev <leonibel.ramirez@gmail.com>
2020-03-25 13:32:09 -0400

    first comm

--- postgres-client/db_config.js
+++ postgres-client/db_config.js
@@ -1,5 +1,17 @@
+/*
+  #CONTACT ME
+  user: Leonibel SR 
+  email: leonibel.ramirez@gmail.com
+*/
+
+//-----------------------------------------
+// intall the module pg
+// npm install -g--save pg
+
+// require module (pg)
 const { Pool, Client } = require('pg');
 
+// declare the class for connection (pool)
 const pool = new Pool({
         user: '',
         host: '',
@@ -8,4 +20,7 @@ const pool = new Pool({
         port: 
     });
 
+
+
+// export the class for use from others files
 module.exports = pool;
